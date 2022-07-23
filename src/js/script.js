bab28037de0ee7eb2cf843dcfa40c63bc9069c1b
Kirill Khilchuk <khilchuk.kirill@gmail.com>
2022-07-23 11:36:28 +0300

    media commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,24 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-/*         adaptiveHeight: true, */
-        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ] 
-      });
-  });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive:{
+      100: {
+        nav: true
+      },
+      1101: {
+        nav: false
+      }
+    }
+   });
+
+  document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  }
+
+    document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+    }
